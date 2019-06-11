c89dc5ee4bca14c8ba895b70f47b0260aae76c2b
Ben Hakes <bhhakes@gmail.com>
2019-06-11 09:19:09 -0500

    add path to dependencies

--- Package.swift
+++ Package.swift
@@ -14,9 +14,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Functional",
-            dependencies: []),
+            dependencies: [],
+	    path: "."),
         .testTarget(
             name: "FunctionalTests",
-            dependencies: ["Functional"]),
+            dependencies: ["Functional"],
+	    path: "."),
     ]
 )
